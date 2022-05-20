00aef1d86ac535195181c8ca1228b7fe873bda8c
Riley Altman <rwaltman@ucsc.edu>
2022-05-19 18:35:37 -0700

    fixed the matrix exponential function to use taylor approximation

--- Lab4/DCM_toEuler_lib/Matrix.h
+++ Lab4/DCM_toEuler_lib/Matrix.h
@@ -19,12 +19,14 @@ float MATRIX_GetValue(Matrix m, int row, int col);
 int MATRIX_SetValue(Matrix m, int row, int col, float value);
 Matrix MATRIX_GetRowVector(Matrix m, int row);
 Matrix MATRIX_GetColumnVector(Matrix m, int col);
+Matrix MATRIX_ConstructIdentity(int size);
 
 Matrix MATRIX_Multiply(Matrix m, Matrix n);
 Matrix MATRIX_Subtract(Matrix m, Matrix n);
+Matrix MATRIX_Add(Matrix m, Matrix n);
 Matrix MATRIX_MultiplyScalar(Matrix m, float s);
 Matrix MATRIX_Transpose(Matrix m);
-Matrix MATRIX_Exponential(Matrix m);
+Matrix MATRIX_Exponential(Matrix pqr, float p, float q, float r, float deltaT);
 
 Matrix MATRIX_ConstructEulerMatrix(float* angles);
 float* MATRIX_GetEulerAngles(Matrix m);
