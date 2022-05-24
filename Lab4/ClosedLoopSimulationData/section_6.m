45b1991911818a6c8746c7b730ea09d5c2afec60
sriram-ven <sriram.venkataraman00@gmail.com>
2022-05-24 14:50:23 -0700

    section 6 finished

--- Lab4/ClosedLoopSimulationData/section_6.m
+++ Lab4/ClosedLoopSimulationData/section_6.m
@@ -38,7 +38,7 @@ phi_arr = zeros();
 theta_arr = zeros();
 psi_arr = zeros();
 
-for i = 1:1:length(trajectoryData)
+for i = 1:length(trajectoryData)
     currGyro = [wGyro(i,1); wGyro(i,2) ; wGyro(i,3)];
     currMag = [Mag(i,1), Mag(i,2), Mag(i,3)];
     currAccel = [Acc(i,1), Acc(i,2), Acc(i,3)];
@@ -52,8 +52,8 @@ end
 
 subplot(3, 1, 1)
 hold on
-plot(phi_arr)
-plot(trajectoryData(:,9))
+plot(-phi_arr(1:400))
+plot(trajectoryData(1:400,9))
 hold off
 title('Calculated vs Actual Yaw (Without Magnetometer)')
 
@@ -68,7 +68,7 @@ title('Calculated vs Actual Pitch (Without Magnetometer)')
 
 subplot(3,1, 3)
 hold on
-plot(psi_arr)
-plot(trajectoryData(:,7))
+plot(psi_arr(1:400))
+plot(trajectoryData(1:400,7))
 hold off
 title('Calculated vs Actual Roll (Without Magnetometer)')
