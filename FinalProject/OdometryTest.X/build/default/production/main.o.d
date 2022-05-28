1686fac2f832a234d45c9852b94fbbf47efad655
Riley Altman <rwaltman@ucsc.edu>
2022-05-27 20:20:23 -0700

    motor speed test created

--- FinalProject/OdometryTest.X/build/default/production/main.o.d
+++ FinalProject/OdometryTest.X/build/default/production/main.o.d
@@ -1,8 +1,10 @@
 build/default/production/main.o: main.c ../../Common/BOARD.h \
- ../../Common/serial.h ../../Common/timers.h ../../Common/Matrix.h \
- ../../Common/Motors.h ../../Common/Odometry.h
+ ../../Common/AD.h ../../Common/serial.h ../../Common/timers.h \
+ ../../Common/Matrix.h ../../Common/Motors.h ../../Common/Odometry.h
 
 ../../Common/BOARD.h:
+
+../../Common/AD.h:
 
 ../../Common/serial.h:
 
--- FinalProject/OdometryTest.X/main.c
+++ FinalProject/OdometryTest.X/main.c
@@ -2,28 +2,64 @@
 #include <stdio.h>
 #include "xc.h"
 #include "BOARD.h"
+#include "AD.h"
 #include "serial.h"
 #include "timers.h"
 #include "Matrix.h"
 #include "Motors.h"
 #include "Odometry.h"
 
+#define POT_RANGE 1023
+
+int PotToSpeed();
+void Delay(int time);
+
 int main(){
     BOARD_Init();
+    AD_Init();
     SERIAL_Init();
     TIMERS_Init();
     MOTORS_Init();
     ODOMETRY_Init();
     
-    MOTORS_SetSpeed(LEFT_MOTOR, -600);
+    AD_AddPins(AD_A0);
     
-    int prevTime = 0;
-    while(1){
-        int curTime = TIMERS_GetMilliSeconds();
-        if (abs(curTime - prevTime) > 40) {
-            printf("\r%f, %f, %f\n", ODOMETRY_GetPositionX(), ODOMETRY_GetPositionY(), ODOMETRY_GetDirection());
-            prevTime = curTime;
+    int speed = 0;
+    MOTORS_SetSpeed(LEFT_MOTOR, speed);
+    Delay(1000);
+    while(speed <= 1000){
+        speed += 1;
+        MOTORS_SetSpeed(LEFT_MOTOR, speed);
+        for(int i = 0; i < 5; i++){
+            printf("\r%d, %f\n", speed, ODOMETRY_GetLeftWheelSpeed());
+            Delay(10);
         }
     }
+    speed = 0;
+    MOTORS_SetSpeed(LEFT_MOTOR, 0);
+    Delay(1000);
+    
+    while(speed >= -1000){
+        speed -= 1;
+        MOTORS_SetSpeed(LEFT_MOTOR, speed);
+        for(int i = 0; i < 5; i++){
+            printf("\r%d, %f\n", speed, ODOMETRY_GetLeftWheelSpeed());
+            Delay(10);
+        }
+    }
+    
+    MOTORS_SetSpeed(LEFT_MOTOR, 0);
+    while(1);
     BOARD_End();
+}
+
+void Delay(int time){
+    int doneTime = TIMERS_GetMilliSeconds() + time;
+    while(TIMERS_GetMilliSeconds() < doneTime);
+}
+
+int PotToSpeed(){
+    int potVal = AD_ReadADPin(AD_A0);
+    int speed = (potVal - (POT_RANGE / 2)) * 2 * MAX_MOTOR_SPEED / POT_RANGE;
+    return speed;
 }
