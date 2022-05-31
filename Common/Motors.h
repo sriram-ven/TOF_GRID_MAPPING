7e2537e23f049c4280e0fe9bfd4254798632bd69
Riley William Altman <rwaltman@ucsc.edu>
2022-05-31 19:33:50 +0000

    Update Common/Motors.h

--- Common/Motors.h
+++ Common/Motors.h
@@ -1,6 +1,10 @@
 #ifndef MOTORS_H
 #define MOTORS_H
 
+typedef enum{
+    POSITION_TRACKING, SPEED_TRACKING,
+}MotorMode;
+
 #define LEFT_MOTOR 0
 #define RIGHT_MOTOR 1
 
@@ -10,11 +14,16 @@
 #define MAX_MOTOR_SPEED 1000
 #define ENCODER_TICKS_PER_REVOLUTION 2160
 
-char MOTORS_Init(void);
+char MOTORS_Init(MotorMode mode);
 
 char MOTORS_SetSpeed(char motor, int dc);
 char MOTORS_SetDirection(char motor, char direction);
+
+// position tracking mode functions
 char MOTORS_SetEncoderCount(char motor, int count);
 int MOTORS_GetEncoderCount(char motor);
+
+// speed tracking mode functions
+float MOTORS_GetMotorSpeed(char motor);
 
 #endif
